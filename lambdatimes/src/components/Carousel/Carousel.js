bdfa12e10cca3a6ef7e5d233289e9209bea83909
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-03-29 11:54:20 -0700

    Added functionality to the Carousel component

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,35 +1,62 @@
-import React, { Component } from 'react';
-import { carouselData } from '../../data';
-// Complete this Carousel 
+import React, { Component } from "react";
+import { carouselData } from "../../data";
+// Complete this Carousel
 export default class Carousel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-
-    }
+      carousel: [],
+      displayedImage: "",
+      imageIndex: 0
+    };
   }
-  componentDidMount(){
-
+  componentDidMount() {
+    this.setState({ carousel: carouselData });
   }
 
   leftClick = () => {
-
-  }
+    let newIndex;
+    if (this.state.imageIndex === 0) {
+      newIndex = 3;
+    } else {
+      newIndex = this.state.imageIndex - 1;
+    }
+    this.setState({
+      imageIndex: newIndex,
+      displayedImage: this.selectedImage(this.state.carousel[newIndex])
+    });
+  };
 
   rightClick = () => {
+    let newIndex;
+    if (this.state.imageIndex === 3) {
+      newIndex = 0;
+    } else {
+      newIndex = this.state.imageIndex + 1;
+    }
+    this.setState({
+      imageIndex: newIndex,
+      displayedImage: this.selectedImage(this.state.carousel[newIndex])
+    });
+  };
 
-  }
+  selectedImage = imageUrl => {
+    return (
+      <img src={imageUrl} style={{ display: "block" }} alt="carousel images" />
+    );
+  };
 
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
-  
-  render(){
+  render() {
     return (
       <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
+        <div className="left-button" onClick={this.leftClick}>
+          {"<"}
+        </div>
+        {this.state.displayedImage}
+        <div className="right-button" onClick={this.rightClick}>
+          {">"}
+        </div>
       </div>
-    )
+    );
   }
 }
